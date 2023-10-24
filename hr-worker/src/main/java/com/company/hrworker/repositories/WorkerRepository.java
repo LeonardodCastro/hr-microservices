fb692e214ddbb8ef9053448481a763ba0d848723
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-24 20:07:16 -0300

    moving

---
+++ hr-worker/src/main/java/com/company/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,10 @@
+package com.company.hrworker.repositories;
+
+
+import com.company.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
--- hr-worker/src/test/java/com/company/hrworker/HrWorkerApplicationTests.java
+++ hr-worker/src/test/java/com/company/hrworker/HrWorkerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.company.hrworker;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EntityScan("com.company.hrworker.entities")
 class HrWorkerApplicationTests {
 
 	@Test
