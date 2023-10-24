0dc3f2282052d5a7ced623560cbf408973b04e31
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-24 19:57:55 -0300

    moving

--- hr-worker/src/main/java/com/company/hrworker/entities/Worker.java
+++ hr-worker/src/main/java/com/company/hrworker/entities/Worker.java
@@ -1,9 +1,15 @@
 package com.company.hrworker.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb_worker")
 public class Worker implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Double dailyIncome;
