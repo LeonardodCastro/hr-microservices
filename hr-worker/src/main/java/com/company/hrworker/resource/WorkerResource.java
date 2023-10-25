d04aa18f83f39b9e1d2035efeb65d39a209b828b
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-25 17:56:54 -0300

    findById endpoint

--- hr-worker/src/main/java/com/company/hrworker/resource/WorkerResource.java
+++ hr-worker/src/main/java/com/company/hrworker/resource/WorkerResource.java
@@ -5,10 +5,12 @@ import com.company.hrworker.repositories.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/workers")
@@ -21,5 +23,10 @@ public class WorkerResource {
     public ResponseEntity<List<Worker>> findAll(){
         List<Worker> workerList = workerRepository.findAll();
         return ResponseEntity.ok(workerList);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id){
+        Worker worker = workerRepository.findById(id).get();
+        return ResponseEntity.ok(worker);
     }
 }
