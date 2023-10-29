f56e46f9b865bada5ec800ef718637da8201b1ff
LeonardodCastro <dev.leonardocastro@gmail.com>
2023-10-29 12:25:48 -0300

    user repository

---
+++ hr-user/src/main/java/com/company/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.company.hruser.repositories;
+
+import com.company.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
